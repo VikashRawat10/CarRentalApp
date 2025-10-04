a790a255ac814a29d0c8e37fdcb5c5f8396ac9d9
VikashRawat10 <vikashraw722@gmail.com>
2025-10-04 11:37:21 +0530

    Update UserRepository.java

--- server/src/main/java/com/wecp/car_rental_management_system/repository/UserRepository.java
+++ server/src/main/java/com/wecp/car_rental_management_system/repository/UserRepository.java
@@ -1,3 +1,13 @@
+// package com.wecp.car_rental_management_system.repository;
+
+
+// import com.wecp.car_rental_management_system.entity.User;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
+
+// public interface UserRepository {
+//     // implement jpa repository here
+// }
 package com.wecp.car_rental_management_system.repository;
 
 
@@ -5,6 +15,8 @@ import com.wecp.car_rental_management_system.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-public interface UserRepository {
-    // implement jpa repository here
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+    User findByUsername(String username);
 }
+
