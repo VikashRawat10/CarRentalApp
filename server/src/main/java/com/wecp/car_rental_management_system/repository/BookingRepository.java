0f5ef96349b0b11e50e24c4cc8a73ec9523b6895
VikashRawat10 <vikashraw722@gmail.com>
2025-10-04 11:34:53 +0530

    Update BookingRepository.java

--- server/src/main/java/com/wecp/car_rental_management_system/repository/BookingRepository.java
+++ server/src/main/java/com/wecp/car_rental_management_system/repository/BookingRepository.java
@@ -1,11 +1,28 @@
+// package com.wecp.car_rental_management_system.repository;
+
+
+// import com.wecp.car_rental_management_system.entity.Booking;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
+
+
+// public interface BookingRepository {
+//     // implement jpa repository here
+// }
+
 package com.wecp.car_rental_management_system.repository;
 
 
 import com.wecp.car_rental_management_system.entity.Booking;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
-public interface BookingRepository {
+@Repository
+public interface BookingRepository extends JpaRepository<Booking,Long>{
     // implement jpa repository here
+
+    List<Booking> getBookingsByUserId(Long userId);
 }
