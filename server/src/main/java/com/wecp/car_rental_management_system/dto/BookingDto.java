f8c29c6eaa5daeca112fd0a731699976d8827a5e
VikashRawat10 <vikashraw722@gmail.com>
2025-10-04 11:21:52 +0530

    Update BookingDto.java

--- server/src/main/java/com/wecp/car_rental_management_system/dto/BookingDto.java
+++ server/src/main/java/com/wecp/car_rental_management_system/dto/BookingDto.java
@@ -1,3 +1,36 @@
+// package com.wecp.car_rental_management_system.dto;
+
+
+// import com.fasterxml.jackson.annotation.JsonFormat;
+
+// import java.util.Date;
+
+// public class BookingDto {
+
+//     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+//     private Date rentalStartDate;
+
+//     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+//     private Date rentalEndDate;
+
+//     public Date getRentalStartDate() {
+//         return rentalStartDate;
+//     }
+
+//     public void setRentalStartDate(Date rentalStartDate) {
+//         this.rentalStartDate = rentalStartDate;
+//     }
+
+//     public Date getRentalEndDate() {
+//         return rentalEndDate;
+//     }
+
+//     public void setRentalEndDate(Date rentalEndDate) {
+//         this.rentalEndDate = rentalEndDate;
+//     }
+// }
+
+
 package com.wecp.car_rental_management_system.dto;
 
 
@@ -29,3 +62,4 @@ public class BookingDto {
         this.rentalEndDate = rentalEndDate;
     }
 }
+
