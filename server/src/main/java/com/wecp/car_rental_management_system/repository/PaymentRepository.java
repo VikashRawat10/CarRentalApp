7fd098129fbe68eff2ba9b9a1fe5ffb8c9334fa1
VikashRawat10 <vikashraw722@gmail.com>
2025-10-04 11:36:51 +0530

    Update PaymentRepository.java

--- server/src/main/java/com/wecp/car_rental_management_system/repository/PaymentRepository.java
+++ server/src/main/java/com/wecp/car_rental_management_system/repository/PaymentRepository.java
@@ -1,3 +1,15 @@
+// package com.wecp.car_rental_management_system.repository;
+
+
+// import com.wecp.car_rental_management_system.entity.Payment;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
+
+// public interface PaymentRepository {
+//     // implement jpa repository here
+// }
+
+
 package com.wecp.car_rental_management_system.repository;
 
 
@@ -5,7 +17,11 @@ import com.wecp.car_rental_management_system.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-public interface PaymentRepository {
-    // implement jpa repository here
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment,Long> {
+
+    Payment findPaymentByBookingId(Long bookingId);
+
 }
 
