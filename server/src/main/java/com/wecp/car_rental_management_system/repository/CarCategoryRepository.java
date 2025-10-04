ccc296d61cc7cdd6a924ac83e926d33557a3734a
VikashRawat10 <vikashraw722@gmail.com>
2025-10-04 11:35:16 +0530

    Update CarCategoryRepository.java

--- server/src/main/java/com/wecp/car_rental_management_system/repository/CarCategoryRepository.java
+++ server/src/main/java/com/wecp/car_rental_management_system/repository/CarCategoryRepository.java
@@ -1,3 +1,15 @@
+// package com.wecp.car_rental_management_system.repository;
+
+
+// import com.wecp.car_rental_management_system.entity.CarCategory;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
+
+// public interface CarCategoryRepository {
+//     // implement jpa repository here
+// }
+
+
 package com.wecp.car_rental_management_system.repository;
 
 
@@ -5,7 +17,8 @@ import com.wecp.car_rental_management_system.entity.CarCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-public interface CarCategoryRepository {
+@Repository
+public interface CarCategoryRepository extends JpaRepository<CarCategory,Long> {
     // implement jpa repository here
 }
 
