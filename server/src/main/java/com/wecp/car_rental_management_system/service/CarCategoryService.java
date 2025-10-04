f8d7f37c07b06244c73c56fa00b3c227ef8438d9
VikashRawat10 <vikashraw722@gmail.com>
2025-10-04 11:41:39 +0530

    Update CarCategoryService.java

--- server/src/main/java/com/wecp/car_rental_management_system/service/CarCategoryService.java
+++ server/src/main/java/com/wecp/car_rental_management_system/service/CarCategoryService.java
@@ -1,3 +1,19 @@
+// package com.wecp.car_rental_management_system.service;
+
+// import com.wecp.car_rental_management_system.entity.CarCategory;
+// import com.wecp.car_rental_management_system.repository.CarCategoryRepository;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
+
+// import java.util.List;
+// import java.util.Optional;
+
+
+// public class CarCategoryService {
+//     // implement car category service
+// }
+
+
 package com.wecp.car_rental_management_system.service;
 
 import com.wecp.car_rental_management_system.entity.CarCategory;
@@ -6,10 +22,45 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
-
+@Service
 public class CarCategoryService {
     // implement car category service
+    @Autowired
+    private CarCategoryRepository carCategoryRepository;
+
+    // Add a new car category
+    public CarCategory addCarCategory(CarCategory carCategory) {
+        return carCategoryRepository.save(carCategory);
+    }
+
+    // Get all car categories
+    public List<CarCategory> getAllCarCategories() {
+        return carCategoryRepository.findAll();
+    }
+
+    // Get a car category by ID
+    public CarCategory getCarCategoryById(Long id) {
+        return carCategoryRepository.findById(id).orElse(null);
+    }
+
+    // Update a car category
+    public CarCategory updateCarCategory(Long id, CarCategory updatedCarCategory) {
+        CarCategory existingCarCategory = carCategoryRepository.findById(id).orElse(null);
+        if (existingCarCategory != null) {
+            existingCarCategory.setName(updatedCarCategory.getName());
+            existingCarCategory.setDescription(updatedCarCategory.getDescription());
+            existingCarCategory.setBaseRate(updatedCarCategory.getBaseRate());
+            return carCategoryRepository.save(existingCarCategory);
+        }
+        return null;
+    }
+
+    // Delete a car category
+    public void deleteCarCategory(Long id) {
+        carCategoryRepository.deleteById(id);
+    }
 }
+
+
 
