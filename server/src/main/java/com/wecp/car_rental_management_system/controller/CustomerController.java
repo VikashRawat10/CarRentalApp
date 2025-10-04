994cc52bfa86f0ef2988f371760ee3bbe31bd931
VikashRawat10 <vikashraw722@gmail.com>
2025-10-04 11:11:04 +0530

    Update CustomerController.java

--- server/src/main/java/com/wecp/car_rental_management_system/controller/CustomerController.java
+++ server/src/main/java/com/wecp/car_rental_management_system/controller/CustomerController.java
@@ -1,3 +1,37 @@
+// package com.wecp.car_rental_management_system.controller;
+
+
+// import com.wecp.car_rental_management_system.dto.BookingDto;
+// import com.wecp.car_rental_management_system.entity.Booking;
+// import com.wecp.car_rental_management_system.entity.Car;
+// import com.wecp.car_rental_management_system.service.BookingService;
+// import com.wecp.car_rental_management_system.service.CarService;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.*;
+
+// import java.time.LocalDateTime;
+// import java.util.List;
+// import java.util.Optional;
+
+
+// public class CustomerController {
+//     @GetMapping("/api/customers/cars/available")
+//     public ResponseEntity<List<Car>> getAvailableCars() {
+//         // get all available cars.
+//         // note: return all the cars where car status is "available"
+//     }
+
+//     @PostMapping("/api/customers/booking")
+//     public ResponseEntity<Booking> bookCar(@RequestParam Long userId, @RequestParam Long carId,
+//                                            @RequestBody BookingDto bookingDto) {
+//         // book a car
+//     }
+
+// }
+
+
 package com.wecp.car_rental_management_system.controller;
 
 
@@ -11,22 +45,35 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
-
+@RestController
 public class CustomerController {
+
+    @Autowired
+    private CarService carService;
+
+    @Autowired
+    private BookingService bookingService;
+
     @GetMapping("/api/customers/cars/available")
     public ResponseEntity<List<Car>> getAvailableCars() {
         // get all available cars.
         // note: return all the cars where car status is "available"
+        List<Car> availabCars = carService.getAvailableCars();
+        return ResponseEntity.ok(availabCars);
     }
 
     @PostMapping("/api/customers/booking")
     public ResponseEntity<Booking> bookCar(@RequestParam Long userId, @RequestParam Long carId,
                                            @RequestBody BookingDto bookingDto) {
         // book a car
+        return new ResponseEntity<Booking>(bookingService.bookCar(userId, carId, bookingDto), HttpStatus.OK);
+    }
+
+    @GetMapping("/api/customers/bookings/{userId}")
+    public ResponseEntity<List<Booking>> getBookingsByUserId(@PathVariable Long userId){
+        return new ResponseEntity<>(bookingService.getBookingsByUserId(userId), HttpStatus.OK);
     }
 
 }
