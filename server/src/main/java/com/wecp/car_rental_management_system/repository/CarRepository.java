404dc6b2441aeb282148012be8249cacf9c91a5a
VikashRawat10 <vikashraw722@gmail.com>
2025-10-04 11:36:01 +0530

    Update CarRepository.java

--- server/src/main/java/com/wecp/car_rental_management_system/repository/CarRepository.java
+++ server/src/main/java/com/wecp/car_rental_management_system/repository/CarRepository.java
@@ -1,3 +1,14 @@
+// package com.wecp.car_rental_management_system.repository;
+
+// import com.wecp.car_rental_management_system.entity.Car;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
+
+// import java.util.List;
+
+// public interface CarRepository {
+//     // implement jpa repository here
+// }
 package com.wecp.car_rental_management_system.repository;
 
 import com.wecp.car_rental_management_system.entity.Car;
@@ -6,6 +17,9 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface CarRepository {
+@Repository
+public interface CarRepository extends JpaRepository<Car, Long>{
     // implement jpa repository here
+    List<Car> findByStatus(String status);
 }
+
