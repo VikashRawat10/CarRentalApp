624e528bf647d0e191dafa66be3627f11824764f
VikashRawat10 <vikashraw722@gmail.com>
2025-10-04 11:23:32 +0530

    Update LoginRequest.java

--- server/src/main/java/com/wecp/car_rental_management_system/dto/LoginRequest.java
+++ server/src/main/java/com/wecp/car_rental_management_system/dto/LoginRequest.java
@@ -1,33 +1,66 @@
+// package com.wecp.car_rental_management_system.dto;
+
+
+// import com.fasterxml.jackson.annotation.JsonCreator;
+// import com.fasterxml.jackson.annotation.JsonProperty;
+
+// public class LoginRequest {
+
+//     private String username;
+//     private String password;
+
+//     @JsonCreator
+//     public LoginRequest(@JsonProperty("username") String username, @JsonProperty("password") String password) {
+//         this.username = username;
+//         this.password = password;
+//     }
+
+//     public String getUsername() {
+//         return username;
+//     }
+
+//     public void setUsername(String username) {
+//         this.username = username;
+//     }
+
+//     public String getPassword() {
+//         return password;
+//     }
+
+//     public void setPassword(String password) {
+//         this.password = password;
+//     }
+// }
+
+
 package com.wecp.car_rental_management_system.dto;
 
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
-public class LoginRequest {
+import java.util.Date;
 
-    private String username;
-    private String password;
+public class BookingDto {
 
-    @JsonCreator
-    public LoginRequest(@JsonProperty("username") String username, @JsonProperty("password") String password) {
-        this.username = username;
-        this.password = password;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date rentalStartDate;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date rentalEndDate;
+
+    public Date getRentalStartDate() {
+        return rentalStartDate;
     }
 
-    public String getUsername() {
-        return username;
+    public void setRentalStartDate(Date rentalStartDate) {
+        this.rentalStartDate = rentalStartDate;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public Date getRentalEndDate() {
+        return rentalEndDate;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
+    public void setRentalEndDate(Date rentalEndDate) {
+        this.rentalEndDate = rentalEndDate;
     }
 }
